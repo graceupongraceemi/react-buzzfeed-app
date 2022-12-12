16a5d3a2d50ff578fad1f3a9e04d991e33ae4b47
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-11 22:22:11 -0800

    In App.js, useState and useEffect as well as async to fetchData are added to receive data from db.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState, useEffect } from 'react';
+
 import Title from './components/Title';
 
 const App = () => {
+  const [quiz, setQuiz] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/quiz');
+      const json = await response.json();
+      console.log(json);
+      setQuiz(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Title />

ca783561598f6220ce9ef388b99376186e880c7f
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-13 13:16:20 -0800

    button question-block for each image with unsplash link is added in QuestionBlock.jsx. In index.css, question-block styling is added for image blocks

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,10 +1,10 @@
 const QuestionBlock = ({ question }) => {
   return (
     <button className='question-block'>
-      <img alt='' />
+      <img src={question.image} alt={question.alt} />
       <h3>{question.text}</h3>
       <p>
-        <a href={question.url}>{question.credit}</a>
+        <a href={question.image}>{question.credit} </a>
         <a href='https://www.unsplash.com'>Unsplash</a>
       </p>
     </button>

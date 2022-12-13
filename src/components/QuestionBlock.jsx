7e32bbd7a9fb875944897a44c8e1010543b29079
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-13 12:52:35 -0800

    In QuestionsBlock.jsx, key _index and question are added in individiaul QuestionBlock component.  In QuestionBlock.jsx, image is added referenced the source

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,5 +1,14 @@
-const QuestionBlock = () => {
-  return <div>QuestionBlock</div>;
+const QuestionBlock = ({ question }) => {
+  return (
+    <button className='question-block'>
+      <img alt='' />
+      <h3>{question.text}</h3>
+      <p>
+        <a href={question.url}>{question.credit}</a>
+        <a href='https://www.unsplash.com'>Unsplash</a>
+      </p>
+    </button>
+  );
 };
 
 export default QuestionBlock;
--- src/components/QuestionsBlock.jsx
+++ src/components/QuestionsBlock.jsx
@@ -1,3 +1,5 @@
+import QuestionBlock from './QuestionBlock';
+
 const QuestionsBlock = ({ quizItem }) => {
   // console.log(quizItem);
 
@@ -6,7 +8,11 @@ const QuestionsBlock = ({ quizItem }) => {
       <h2 id={quizItem.id} className='question-title'>
         {quizItem.text}
       </h2>
-      <div className='questions-container'></div>
+      <div className='questions-container'>
+        {quizItem.questions.map((question, _index) => (
+          <QuestionBlock key={_index} question={question} />
+        ))}
+      </div>
     </>
   );
 };
